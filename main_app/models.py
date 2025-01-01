2b6ea80b58bac002e693b402e288504ffc7699bc
Metricgarlic <rinkitokun@gmail.com>
2025-01-01 12:02:34 +0800

    Remove the student ID from the Person

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 
 # Base abstract model
 class Person(AbstractBaseUser):
-    student_id = models.CharField(max_length=10)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
